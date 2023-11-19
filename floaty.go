f03fd3d0d7754d81e0b7a57ead74e1294e51b425
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-11-19 10:24:03 +0000

    Basic shape.

--- floaty.go
+++ floaty.go
@@ -3,23 +3,32 @@
 package floaty
 
 import (
-	"net/http"
-	"strconv"
-
+	//"strconv"
 	"github.com/caddyserver/caddy/v2"
-	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
-	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
-	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
-	nanoid "github.com/matoous/go-nanoid/v2"
+	//"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	//nanoid "github.com/matoous/go-nanoid/v2"
 )
 
 // Floaty implements global placeholders that rolls with a set interval.
 type FloatyID struct {
 	// Length of instance ID
-	Length int `json:"length"`;
+	Length int `json:"length"`
 
 	// Map of additional instance IDs to be set
-	Additional map[string]int `json:"additional,omitempty"`;
+	Additional map[string]int `json:"additional,omitempty"`
 }
 
 // Initialize the module.
+func init() {
+	caddy.RegisterModule(FloatyID{})
+}
+
+// Register the Caddy plugin.
+func (FloatyID) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID: "caddy.system.floaty",
+		New: func() caddy.Module {
+			return new(FloatyID)
+		},
+	};
+}
